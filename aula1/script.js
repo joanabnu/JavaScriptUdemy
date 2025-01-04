02015711009579c01fbc10fed58641192da57515
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-01-04 14:03:38 -0300

    variaveis e console

--- aula1/script.js
+++ aula1/script.js
@@ -1,1 +1,22 @@
-alert('cheguei')
+alert("cheguei")
+
+console.log("joana")
+console.warn("ERRO 300")
+console.error("ERRO 550") // mensagem de erro vermelho
+
+console.group("App") // inicio do grupo
+console.log("Joana")
+console.warn("Erro 300") // Mensagem de erro em amarelo
+console.groupEnd() // final do grupo
+
+// Variaveis
+// var(antigo) let(novo) const
+// camelCase
+
+let firstName = "Joana"
+let lastName = "Maria"
+let age = 24
+console.log(firstName, lastName, age)
+
+const calcTax = 0.7
+console.log(calcTax)
