9ea3b1bf8271484b1edd53e3a26b7227ee997e61
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-01-05 18:46:13 -0300

    parseInt, parseFloat, Boolean,Number

---
+++ aula5/script.js
@@ -0,0 +1,44 @@
+// Static ou Dymanic
+
+// Static
+// C, C++,Java,TypeScript
+
+// Dymanic
+// Python, JavaScript,Ruby,PHP
+let firstName = "Andre"
+let age = 35
+
+// Convertions ou Casting
+
+let idade = "24"
+/*console.log(idade, typeof idade)
+
+// ParseInt (inteiro)
+idade = parseInt(idade)
+console.log(idade, typeof idade)
+
+// ParseFloat (Fracionario)
+idade = parseFloat(idade)
+console.log(idade, typeof idade)*/
+
+// Unsrio (+) ele faz o funcao do parseInt e do parseFloat
+idade = +idade
+console.log(idade, typeof idades)
+
+// Number
+idade = Number(idade)
+console.log(idade, typeof idade)
+
+// Number para String
+
+let alter = 40
+//alter = alter.toString() primeira forma
+//alter = String(alter) segunda forma
+console.log(alter, typeof alter)
+
+// Number para Boolean (true ou false)
+// Number 0 = false
+// Number 1 etc true
+
+alter = Boolean(alter)
+console.log(alter, typeof alter)
