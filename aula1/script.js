5e00a18c18adac3e9d01ee43f2ab8389c60bfd16
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-01-05 18:06:52 -0300

    Date types

--- aula1/script.js
+++ aula1/script.js
@@ -20,3 +20,44 @@ console.log(firstName, lastName, age)
 
 const calcTax = 0.7
 console.log(calcTax)
+
+// Data Types
+// primitive - reference
+// string
+firstName = "Joana"
+console.log(firstName, typeof firstName)
+// Number
+
+// Boolean
+let isadult = false
+// null
+let address = null
+console.log(address, typeof address)
+// undefined
+let color
+console.log(color, typeof color)
+
+// symbol
+let id = Symbol("id")
+let meuObjeto = {
+  [id]: 123456,
+}
+
+console.log(meuObjeto[id])
+
+let num1 = "10"
+let num2 = "2"
+console.log(num1 + num2)
+
+// reference Data Types
+let number = [10, 20, 30, 35, 50]
+console.log(number, typeof number)
+
+function myName() {
+  console.log("Andre")
+}
+console.log(myName, typeof myName)
+
+//Date
+let agora = new Date()
+console.log(agora)
