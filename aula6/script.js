38b18e9aa13055d0d83e6aa187daacfcd12bdcd3
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-01-09 10:38:12 -0300

    Operadores aritmetricos e atribuição

---
+++ aula6/script.js
@@ -0,0 +1,41 @@
+// Operadores Aritmetricos
+
+let soma = 6 + 6
+let Subtracao = 6 - 10
+let multiplicacao = 6 * 6
+let divisao = 6 / 6
+let resto = 6 % 6
+
+console.log(
+  `Soma : ${soma},Subtração :  ${Subtracao}, Multiplicação : ${multiplicacao}, Divisão : ${divisao} Resto : ${resto}`
+)
+
+// incremento
+soma++
+console.log(` Incremento : ${soma}`)
+
+// Decremento
+Subtracao--
+console.log(`Descremento : ${Subtracao}`)
+
+// Exponencia
+let explonenciação = 2 ** 3
+console.log(`Exponenciação : ${explonenciação}`)
+
+// Operadores de Atribuição
+let val1 = 10
+let val2 = 10
+let val3 = 10
+let val4 = 10
+let val5 = 10
+let val6 = 10
+val1 += 5 // adiçao
+val2 -= 5 // Subtração
+val3 *= 5 // multiplição
+val4 /= 5 // divisao
+val5 %= 5 // Resto
+val6 **= 3 // exponenciação
+
+console.log(
+  ` Soma :  ${val1} Subtração : ${val2} Multipliçao : ${val3} Divisão : ${val4} Resto : ${val5} Esponenciação : ${val6}`
+)
