a77d8e7454f1828327dea3c067d83f2bdfb11c92
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-01-14 12:10:29 -0300

    Date
    Transformando data de acordo com cada idioma
    criando contador para dias especificos
    dias,mes e ano

--- aula9/script.js
+++ aula9/script.js
@@ -8,3 +8,29 @@ console.log(dataEspecifica)
 
 let dataString = new Date("2000/11/26 23:35")
 console.log(dataString)
+
+let data = new Date()
+// O dia do Mes
+// data.setDate(10) alterar o dia
+console.log(data.getDate())
+// O dia da Semana (0-6) onde 0 é dmingo
+console.log(data.getDay())
+// O mes (0-11) onde 0 é janeiro
+//data.setMonth(0) alterar mes
+console.log(data.getMonth())
+// O ano
+console.log(data.getFullYear())
+
+// Data e Hora
+// Criar um contador de dia para uma data especifica. Inicio, Fim, a diferença de dias entre essas datas
+
+let inicio = new Date("2000/11/26")
+let fim = new Date("2025/01/14")
+let totalDias = (fim - inicio) / (1000 * 3600 * 24)
+console.log(totalDias)
+
+data = Intl.DateTimeFormat("de-DE").format(data)
+console.log(data)
+
+//data = Intl.DateTimeFormat("fr-FR").format(data)
+//console.log(data)
