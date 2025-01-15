fc91dbdf40dd6d742003f20dd89c89921110653d
Joana Maria <89950312+joanabnu@users.noreply.github.com>
2025-01-15 12:27:53 -0300

    Array

---
+++ aula10/script.js
@@ -0,0 +1,29 @@
+//Arrays
+let cart = ["Agua", "Arroz", "Carne", "feijao"]
+console.log(cart)
+
+console.log(`A minha comida favorita é ${cart[1]} e ${cart[3]} `)
+console.log(`A minha  lista de compras para hoje contem os itens : ${cart}`)
+
+//scart[1] = "Laranja"
+console.log(cart)
+
+let petShop = ["Dogs", "Cats", "Birds", "Hamters"]
+
+console.log(`${petShop[2]}`)
+
+let numbers = [10, 20, 33, 40, 5, 13, true]
+
+cart[4] = "Sucos"
+console.log(cart.length) // saber quantidade
+cart.push("Suco") // adicionar ao final
+cart.pop() // remove o ultimo item
+cart.shift // remove o primeiro item
+cart.unshift("batata") // adicionar o item no incio
+cart.sort() // ele vai organizar em ordem alfabetica
+console.log(cart)
+let y
+// y = cart. includes('Agua') // Verifica se existe esse item na listas()arrays
+
+y = cart.indexOf("Arroz")
+console.log(y)
